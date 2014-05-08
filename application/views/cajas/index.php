8116860e18ecbef051de7ba3a0e0a8e628cacb92
502289789 <obed.9@hotmail.com>
2014-05-07 23:48:50 -0500

    change the styles and add bootstrap to the page

--- application/views/cajas/index.php
+++ application/views/cajas/index.php
@@ -10,12 +10,21 @@
         
         $('#estadoDeCuenta').click(function(){
             $('#main-content').load("<?php echo base_url()?>index.php/vistas/estado_de_cuenta_C_view"); 
+            $(this).addClass("active");
+            $('#reportes').removeClass("active");
+            $('#conceptos').removeClass("active");
         });
         $('#reportes').click(function(){
             $('#main-content').load("<?php echo base_url()?>index.php/vistas/reportes_C_view"); 
+            $(this).addClass("active");
+            $('#estadoDeCuenta').removeClass("active");
+            $('#conceptos').removeClass("active");
         });
         $('#conceptos').click(function(){
             $('#main-content').load("<?php echo base_url()?>index.php/vistas/conceptos_C_view"); 
+            $(this).addClass("active");
+            $('#reportes').removeClass("active");
+            $('#estadoDeCuenta').removeClass("active");
         });
     });
     
@@ -26,7 +35,7 @@
     }
 </style>
     
-<div id="mainMenu">
+<!--<div id="mainMenu">
     <div id="estadoDeCuenta" class="buttonMenu">
         <span>Estado de Cuenta</span>
     </div>
@@ -36,7 +45,31 @@
     <div id="conceptos" class="buttonMenu">
         <span>Conceptos</span>
     </div>
-</div>
+</div>-->
+
+<nav class="navbar navbar-default" role="navigation">
+  <div class="container-fluid">
+    <!-- Brand and toggle get grouped for better mobile display -->
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+        <span class="sr-only">Toggle navigation</span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
+      <a class="navbar-brand" href="#">Sistema de Pagos</a>
+    </div>
+
+    <!-- Collect the nav links, forms, and other content for toggling -->
+    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+      <ul class="nav navbar-nav">
+        <li class="active" id="estadoDeCuenta"><a href="#">Estado de Cuenta</a></li>
+        <li id="reportes"><a href="#">Reportes</a></li>
+        <li id="conceptos"><a href="#">Conceptos</a></li>
+      </ul>
+    </div><!-- /.navbar-collapse -->
+  </div><!-- /.container-fluid -->
+</nav>
 <section id="main-content">
     
 </section>
--- application/views/header.php
+++ application/views/header.php
@@ -3,10 +3,12 @@
     <head>
         <meta charset="utf-8"/>
         <title>Sistema de pagos</title>
-        <link rel="stylesheet" href="<?php echo base_url();?>/css/styles.css" />
+        <link rel="stylesheet" href="<?php echo base_url();?>css/styles.css" />
         <link href="<?php echo base_url();?>js/css/ui-lightness/jquery-ui-1.10.4.css" rel="stylesheet">
 	<script src="<?php echo base_url();?>js/js/jquery-1.10.2.js"></script>
 	<script src="<?php echo base_url();?>js/js/jquery-ui-1.10.4.js"></script>
+        <link rel="stylesheet" href="<?php echo base_url();?>componentes/bootstrap/css/bootstrap.css" />
+        <script src="<?php echo base_url();?>componentes/bootstrap/js/bootstrap.js"></script>
         <script>
             $(function(){
                 $('#menu_header').hide();
@@ -18,7 +20,7 @@
     </head>
     <body>
         <header>
-            <div id="h_contain">
+<!--            <div id="h_contain">
                 <div id="h_user_info">
                     <img id="img_user" width="30" src="<?php echo base_url()?>/images/user-default.png" >
                     <div id="info">
@@ -27,7 +29,7 @@
                     </div>
                     
                 </div>
-            </div>
+            </div>-->
             <div id="menu_header" hidden>
                 <div id="boton_cambia_pass" class="buttonUserInfo"><span >Cambiar contraseña</span></div>
                 <div id="boton_perfil" class="buttonUserInfo"><span>Perfil</span></div>
